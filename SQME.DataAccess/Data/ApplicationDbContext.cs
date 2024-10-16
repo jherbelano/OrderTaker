728b52a2404c2810db6aa9a65ee0f6177b918897
Jeramine Belano <yuanasherbelano@gmail.com>
2024-10-16 19:23:10 +0800

    Add project files.

---
+++ SQME.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SQME.Models;
+
+namespace SQME.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ SQME.Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SQME.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
